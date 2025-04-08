a9820d887de9eb18db157b0e36e919fe82a2ea68
shadeyg56 <shadeyg56@gmail.com>
2025-04-07 22:19:09 -0500

    Add status messages to setup script

--- setup.py
+++ setup.py
@@ -4,23 +4,27 @@ import json
 MODEL_NAME = "llama3.2"
 
 email = input("Please enter the email you used for Open-WebUI: ")
-password = input("Please enter your password")
+password = input("Please enter your password: ")
 
 webui = OpenWebUI(email, password)
 
 knowledge = webui.create_or_get_knowledge("SysAdmin Knowledge", "SysAdmin Knowledge scraped from ServerFault")
 
+print("\nDownloading model... might take a while")
 model = webui.download_model(MODEL_NAME)
+print("Download complete")
  
 with open("data.json", "r") as f:
     data = json.load(f)
 entries = []
 for entry in data:
     string = f"{entry["title"]}\n{entry["top_answer"]}"
-    entries.append(entry["top_answer"])
+    entries.append(string)
 
-ids = webui.add_knowledge(knowledge.get("id"), entries[0:100])
+print("Adding knowledge.. might take a while.")
+ids = webui.add_knowledge(knowledge.get("id"), entries)
 knowledge = webui.create_or_get_knowledge("SysAdmin Knowledge", "SysAdmin Knowledge scraped from ServerFault")
 
-
+print("Configuring model...")
 webui.create_model_from(MODEL_NAME, [knowledge])
+print("Setup complete!")
