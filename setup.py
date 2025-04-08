f71b63194c2599147c8e1e43588d5754136cdd74
shadeyg56 <shadeyg56@gmail.com>
2025-04-07 21:56:41 -0500

    Add  post title to knowledge entries

--- setup.py
+++ setup.py
@@ -14,11 +14,12 @@ model = webui.download_model(MODEL_NAME)
  
 with open("data.json", "r") as f:
     data = json.load(f)
-stuff = []
+entries = []
 for entry in data:
-    stuff.append(entry["top_answer"])
+    string = f"{entry["title"]}\n{entry["top_answer"]}"
+    entries.append(entry["top_answer"])
 
-ids = webui.add_knowledge(knowledge.get("id"), stuff[0:100])
+ids = webui.add_knowledge(knowledge.get("id"), entries[0:100])
 knowledge = webui.create_or_get_knowledge("SysAdmin Knowledge", "SysAdmin Knowledge scraped from ServerFault")
 
 
